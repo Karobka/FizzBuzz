4aebece77e2a5a633dc24599b93d216dc88b8955
Tim Wright <mithgirw@gmail.com>
2016-06-10 22:42:45 -0600

    finished

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
-alert("it works");
-for (var i=0; i<=100; i++) {
-	if (i % 3 === 0) {
+for (var i=1; i<=100; i++) {
+	if (i % 3 === 0 && i % 5 === 0) {
+    	$("#puthere").append('<p>' + 'fizzbuzz' + '</p>');
+      console.log('fizzbuzz');
+  	}else if (i % 3 === 0) {
 		$("#puthere").append('<p>' + 'fizz' + '</p>');
 	  console.log("fizz");
 	}else if (i % 5 === 0) {
 		$("#puthere").append('<p>' + 'buzz' + '</p>');
       console.log("buzz");
-    }else if (i % 3 && i % 5 === 0) {
-    	$("#puthere").append('<p>' + 'fizzbuzz' + '</p>');
-      console.log('fizzbuzz');
     }else {
     	$("#puthere").append('<p>' + i + '</p>');
       console.log(i);
